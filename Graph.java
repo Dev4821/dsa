9d8fecfe331f5d63ef098fca98fe72c2c97ea3e4
Dev4281 <divyansh.sharma0210@gmail.com>
2023-03-28 22:59:24 +0530

    bfs implementation

--- Graph.java
+++ Graph.java
@@ -11,6 +11,9 @@ class Edge{
     }
 }
 public class Graph {
+
+    // For Creating graph:
+
    static  ArrayList <Edge>graph[];
    static int v;
     Graph(int v)
@@ -23,6 +26,8 @@ public class Graph {
        }
     }
     
+    // addEdge : For Adding Edge
+
     public static void addEdge(int src,int dest, int weight)
     {
         Edge e=new Edge(src,dest,weight);
@@ -30,6 +35,8 @@ public class Graph {
        graph[src].add(e);
        
     }
+
+    // Normal Print
     public static void print()
     {
         for(int i=0;i<v;i++)
@@ -42,14 +49,48 @@ public class Graph {
             
         }
     }
+    
+  // BFS implementation:
+    public static void breadthfirstSearch()
+    {
+        boolean visited[]=new boolean[v];
+        for(int i=0;i<v;i++)
+        {
+         BFS(i,visited);
+        }
+    }
+    public static void BFS(int node,boolean visited[])
+    {
+      Queue <Integer> q=new LinkedList<>();
+      
+      q.add(node);
+      while(!q.isEmpty())
+      {
+       int temp=q.poll();
+      
+       if(!visited[temp])
+       {
+        System.out.println(temp);
+        visited[temp]=true;
+        for(int i=0;i<graph[temp].size();i++)
+        {
+          Edge e=graph[temp].get(i);
+          q.add(e.dest);
+        }
+       }
+
+      }
+    }
     public static void main(String args[]) {
       Graph g=new Graph(5);
       addEdge(0,1,2);
       addEdge(1,2,3);
-      addEdge(2,3,5);
+      addEdge(2,1,5);
       addEdge(3,4,2);
       addEdge(2,4,4);
       print();
+      
+      breadthfirstSearch();
       
     }
 }
